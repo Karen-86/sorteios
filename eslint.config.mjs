edd162cf989e451f4f32547da507f19dc986a0b8
Karen <techshift86@gmail.com>
2025-08-03 09:51:42 +0400

    jf

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-      {
+  {
     rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
+      "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "react-hooks/exhaustive-deps": "off",
       "@next/next/no-img-element": "off",
       "jsx-a11y/alt-text": "off",
+      "@next/next/no-html-link-for-pages": "off",
     },
   },
 ];
